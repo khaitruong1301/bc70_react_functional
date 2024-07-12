cfda744f2f2ba0d622dae7915d58a11df47a8c7a
khai <khaitruong1301@gmail.com>
2024-07-12 21:21:28 +0700

    buoi2_state

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,23 @@
 import React from 'react'
 import Databinding from './databinding/Databinding'
 import DemoHandleEvent from './eventHandler/DemoHandleEvent'
+import DemoRenderCondition from './renderCondition/DemoRenderCondition'
+import ChangeNumber from './DemoState/ChangeNumber'
+import ChangeFontSize from './DemoState/ChangeFontSize'
+import TinkerApp from './DemoState/TinkerApp'
+import ChangeCarColor from './DemoState/ChangeCarColor'
 
 const App = () => {
   return (
     <div style={{marginBottom:'500px'}}>
         {/* <Databinding /> */}
-        <DemoHandleEvent />
+        {/* <DemoHandleEvent /> */}
+        {/* <DemoRenderCondition /> */}
+
+        {/* <ChangeNumber /> */}
+        {/* <ChangeFontSize /> */}
+        {/* <TinkerApp /> */}
+        <ChangeCarColor />
 
     </div>
   )
