379989c441ec6ad614c5b41004d5a90b79702a9f
khai <khaitruong1301@gmail.com>
2024-07-26 21:27:57 +0700

    buoi7_react_router_dom_basic

--- src/App.jsx
+++ src/App.jsx
@@ -15,27 +15,36 @@ import EXCarStore from './LiftingStateup/EXCarStore/EXCarStore'
 import ShoesPage from './ApiDemo/ShoesPage'
 import DemoLoginForm from './FormDemo/DemoLoginForm'
 import DemoLoginForm_useFormik from './FormDemo/DemoLoginForm_useFormik'
-
+//Cấu hình react router dom
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage'
+import Login from './pages/Login'
+import Search from './pages/Search'
+import About from './pages/About'
+import Cart from './pages/Cart'
+import Contact from './pages/Contact'
 const App = () => {
-  
+
   return (
-    <div style={{marginBottom:'500px'}}>
-        {/* <Databinding /> */}
-        {/* <DemoHandleEvent /> */}
-        {/* <DemoRenderCondition /> */}
-        {/* <ChangeNumber /> */}
-        {/* <ChangeFontSize /> */}
-        {/* <TinkerApp /> */}
-        {/* <ChangeCarColor /> */}
-        {/* <DemoProps /> */}
-        {/* <RenderWithMap /> */}
-        {/* <BTProductList /> */}
-        {/* <DanhSachSanPham /> */}
-        {/* <EXCarStore /> */}
-        {/* <ShoesPage /> */}
-        {/* <DemoLoginForm /> */}
-        {/* <DemoLoginForm_useFormik /> */}
-    </div>
+    <BrowserRouter>
+      <header className='bg-dark text-white'>
+        <nav className='p-2'>
+          <a href='/' className='mx-2 text-white'>Home</a>
+          <a href='/login' className='mx-2 text-white'>Login</a>
+          <a href='/about' className='mx-2 text-white'>About</a>
+        </nav>
+      </header>
+      <Routes>
+        {/* <Route path='' element={<HomePage />}></Route> */}
+        <Route index element={<HomePage />}></Route>
+        <Route path='home' element={<HomePage />}></Route>
+        <Route path='login' element={<Login />}></Route>
+        <Route path='search' element={<Search />}></Route>
+        <Route path='about' element={<About />}></Route>
+        <Route path='cart' element={<Cart />}></Route>
+        <Route path='contact' element={<Contact />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
